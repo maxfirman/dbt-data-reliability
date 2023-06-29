e0292a4f0ca9614beb9d605fe6d592054543921c
Firman, Max <max.firman@troweprice.com>
2023-06-29 14:08:41 +0100

    Delete value_assertions.sql and quote min

--- integration_tests/macros/e2e_tests/utils/value_assertions.sql
+++
@@ -1,9 +0,0 @@
-{% macro assert_value(actual, expected) %}
-    {% if actual == expected %}
-        {% do elementary.edr_log(context ~ " SUCCESS: " ~ actual  ~ " == " ~ expected) %}
-        {{ return(0) }}
-    {% else %}
-        {% do elementary.edr_log(context ~ " FAILED: " ~ actual ~ " != " ~ expected) %}
-        {{ return(1) }}
-    {% endif %}
-{% endmacro %}
